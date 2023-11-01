e1cca9922d0d42a3c6a0b90a5ea1e39f4ea4dd0c
Radoslaw Draguc <radoxxpl@gmail.com>
2023-11-01 23:06:16 +0100

    HashRouter fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from '../src/components/Navbar'
-import { BrowserRouter, Route ,Routes } from 'react-router-dom';
+import { HashRouter, Route ,Routes } from 'react-router-dom';
 import Contact from './components/Contact'
 import Aboutus from './components/Aboutus'
 import Cars from './components/Cars'
@@ -9,20 +9,20 @@ import Error from './components/Error'
 import Footer from './components/Footer';
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App" basename="/Car-Rental">
+    <HashRouter>
+    <div className="App">
       <Navbar/>
     
     <Routes>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/aboutus'  element={<Aboutus/>}/>
         <Route path='/cars'  element={<Cars/>}/>
-        <Route path='/Car-Rental' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='*' element={<Error/>}/>
         </Routes>
         <Footer/> 
       </div> 
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
